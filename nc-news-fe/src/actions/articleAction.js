fceb066af74cdb6a495851621e67def67e7e5828
FrancisBroughton <iamfrancisbroughton@gmail.com>
2018-02-22 13:27:28 +0000

    article action set up

---
+++ nc-news-fe/src/actions/articleAction.js
@@ -0,0 +1,28 @@
+import axio from 'axios';
+import * as types from '../types';
+ 
+export const fetchAllArticlesRequest= ()=>  ({
+    type: types.FETCH_ALL_ARTICLES_REQUEST
+});
+
+export const fetchAllArticlesSuccess = (data) => ({
+  type: types.FETCH_ALL_ARTICLES_SUCCESS,
+  payload: data
+})
+
+export const fetchAllArticlesFailure = (error) => ({
+  type: types.FETCH_ALL_ARTICLES_FAILURE,
+  payload: error
+})
+
+export function fetchAllArticles(data){
+  return function (dispatch){
+      axios.get('https://northcoders-news-api.herokuapp.com/api/articles')
+      .then(res => {
+          dispatch(fetchAllArticlesSuccess(res.data.articles))
+      })
+      .catch (err => {
+          dispatch(fetchAllArticlesFailure(err))
+      })
+  }
+}
