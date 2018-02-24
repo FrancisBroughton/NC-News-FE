e49a832aec92c4c43c7d486a76579d1b0d45b873
FrancisBroughton <iamfrancisbroughton@gmail.com>
2018-02-24 12:34:40 +0000

    top 10 articles render on home page

--- nc-news-fe/src/components/Homepage.js
+++ nc-news-fe/src/components/Homepage.js
@@ -46,19 +46,19 @@
     }
 }
 
-// function mapStateToProps(state) {
-//     return {
-//         loading: state.articles.loading,
-//         error: state.articles.error,
-// //         articles: state.articles.payload
-// //     };
-// // }
-// function mapDispatchToProps(dispatch) {
-//     return {
-//         fetchAllArticles: (data) => {
-//             dispatch(fetchAllArticles());
-//         }
-//     };
-// }
+function mapStateToProps(state) {
+    return {
+        loading: state.articles.loading,
+        error: state.articles.error,
+        articles: state.articles.payload
+    };
+}
+function mapDispatchToProps(dispatch) {
+    return {
+        fetchAllArticles: (data) => {
+            dispatch(fetchAllArticles());
+        }
+    };
+}
 
 export default connect(mapStateToProps, mapDispatchToProps)(Homepage);
