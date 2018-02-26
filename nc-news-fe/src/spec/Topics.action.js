b234768d0c62d27deab33b9d33754bbf086c3aa0
FrancisBroughton <iamfrancisbroughton@gmail.com>
2018-02-26 10:41:04 +0000

    tests build for topics

---
+++ nc-news-fe/src/spec/Topics.action.js
@@ -0,0 +1,44 @@
+import { expect } from 'chai';
+import * as types from '../types';
+import * as topicsActions from '../actions/topicsAction';
+
+
+describe('Actions for topics', () => { 
+    it('should return the correct action for fetchtopicsRequest', () => {
+        expect(topicsActions.fetchTopicsRequest()).to.eql({
+            type: types.FETCH_TOPICS_REQUEST
+        });
+    });
+
+    it('should return the correct action for fetchTopicsSuccess', () => {
+        const data = [1, 2, 3, 4];
+        expect(topicsActions.fetchTopicsSuccess(data)).to.eql({
+            type: types.FETCH_TOPICS_SUCCESS,
+            payload : data
+        });
+    });
+
+    it('should return the correct action for fetchTopicsFailure', () => {
+        const error = 'error';
+        expect(topicsActions.fetchTopicsFailure(error)).to.eql({
+            type: types.FETCH_TOPICS_FAILURE,
+            error: error
+        });    
+    });
+
+    it('should return the correct action for fetchArticlesByIdSuccess', () => {
+        const data = [1, 2, 3, 4];
+        expect(topicsActions.fetchArticlesByIdSuccess(data)).to.eql({
+            type: types.FETCH_ARTICLES_BY_ID_SUCCESS,
+            topicPayload : data
+        });
+    });
+    
+    it('should return the correct action for fetchArticlesByIdFailure', () => {
+        const error = 'error';
+        expect(topicsActions.fetchArticlesByIdFailure(error)).to.eql({
+            type: types.FETCH_ARTICLES_BY_ID_FAILURE,
+            topicError: error
+        });    
+    });
+});
