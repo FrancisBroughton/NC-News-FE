015c786c7e3768b5ee7899c5c6b5bd1342ae7ad1
FrancisBroughton <iamfrancisbroughton@gmail.com>
2018-02-27 16:57:24 +0000

    removed duplicate

--- nc-news-fe/src/actions/topicAction.js
+++ nc-news-fe/src/actions/topicAction.js
@@ -27,9 +27,3 @@ export function fetchAllTopics(data) {
   }
 }
   
-
-
-
-export const FETCH_TOPICS_REQUEST = 'FETCH_TOPICS_REQUEST';
-export const FETCH_TOPICS_SUCCESS = 'FETCH_TOPICS_SUCCESS';
-export const FETCH_TOPICS_FAILURE = 'FETCH_TOPICS_FAILURE';
